cf2c73e2c84e61fbf016a1b6d72378433778c2bb
Kushalaa Hande <39940258+kushalaa@users.noreply.github.com>
2025-04-08 19:07:35 +0000

    Update settings and views for development environment
    - Set ALLOWED_HOSTS in settings.py to include localhost and development URL.
    - Modify api_root view in views.py to use a base URL for API endpoints.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9evbjlv6&5hc^2b^f0zgy3ik3iefspde=gqr+y5i3e4cgnl!3e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'didactic-barnacle-g79w6qg746fp9qv-8000.app.github.dev']
 
 
 # Application definition
